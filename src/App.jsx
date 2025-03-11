d927e5df2c682fec2186a3c4f71b34cc948ee50e
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-03-11 20:51:03 +0530

    input box added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <div className="bg-blue-300 w-full h-[700px]">
+       <h1 className="text-center text-5xl pt-20">Weather App</h1>
+       <input type="text" className="bg-white w-200 block mx-auto h-14 mt-9 focus:outline-none rounded-4xl text-3xl text-center border-2 border-blue-400" />
+      </div>
+    </div>
+  );
+}
+
+ 
