5a7f99cef0f086d0a690e1400520855123cbd72d
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-03-12 21:27:22 +0530

    weather api set

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,80 @@
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
+import React, { useEffect, useState } from 'react'
+import weatherImage from "./assets/Weather-pana.png"
+import WeatherDetail from "./WeatherDetail"
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <div>
-      const API_KEYS="44f75821f5b433f0de19a4ef400268f3"
-      const [city,setcity] = useState("");
-      const [weather, setWeather] = useState({
-      temp:null,
-      humidity: null,
-      visibility: null,
-      minTemp: null,
-      maxTemp: null,
-      feelsLike: null
-      });
-      const getWeatherData =(()=>{}); 
-
-      useEffect(()=>{ 
-      getWeatherData();
-      }, [city]);
-
-
-      <div className="bg-blue-300 w-full h-[700px]">
-       <h1 className="text-center text-5xl pt-20">Weather App</h1>
-       <input type="text" 
-              className="bg-white w-200 block mx-auto h-14 mt-9 focus:outline-none rounded-4xl text-3xl text-center border-2 border-blue-400"
-              value={city}
-              onChange={(e)=> setcity(e.target.value)}/>
-      </div>
-    </div>
-  );
+const kelvinToCelsius = (fehrenheit) =>{
+  return ((fehrenheit-273.15).toFixed(2))
 }
 
+const App = () => {
+  const API_KEY = "9efc92fba7e8bc6d9d915b83f2c60e9d"; 
+  const [city, setCity] = useState("Pune")
+  const [weather, setWeather] = useState({
+    temp:30,
+    visibility: 10,
+    humidity: 50,
+    minTemp:20,
+    maxTemp: 40,
+    feelsLike:32,
+  })
 
+  "https://api.openweathermap.org/data/2.5/weather?q=London&appid={API_KEY}"
+  const getWeatherData = async() => {
+    const response =await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+    const weatherData = response.data;
+    setWeather({
+      temp: weatherData.main.temp,
+      visibility: weatherData.visibility,
+      humidity: weatherData.main.humidity,
+      minTemp: weatherData.main.temp_min,
+      maxTemp: weatherData.main.temp_max,
+      feelsLike: weatherData.main.feels_like,
+    })
+  };
 
- 
+  useEffect(()=>{
+    getWeatherData();
+  }, [city])
+
+  return (
+    <div className='bg-green-200 min-h-screen'>
+        <img src={weatherImage} className='h-[300px] mx-auto'></img>
+        <input 
+            type='text' 
+            value={city} 
+            onChange={(e) => setCity(e.target.value)}
+            placeholder='Search Your City...'
+            className='border border-green-500 p-2 w-1/2 bg-white mx-auto block text-4xl text-center focus:outline-none rounded-full'
+        />
+
+        <div className='bg-white opacity-80 p-4 mt-4 w-1/2 mx-auto rounded-lg text-2xl'>
+          <WeatherDetail 
+            detail="Temparature" 
+            value={`${kelvinToCelsius(weather.temp)} °C`}
+          />
+
+          <WeatherDetail 
+            detail="Feels Like" 
+            value={`${kelvinToCelsius(weather.feelsLike)} °C`}
+          />
+
+          <WeatherDetail 
+            detail="Min Temp" 
+            value={`${kelvinToCelsius(weather.minTemp)} °C`}
+          />
+
+          <WeatherDetail 
+            detail="Max Temp" 
+            value={`${kelvinToCelsius(weather.maxTemp)} °C`}
+          />
+
+          <WeatherDetail detail="Visiblity" value={`${weather.visibility} Miles`}/>
+
+          <WeatherDetail detail="Humidity" value={`${weather.humidity} %`}/>
+        </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/WeatherDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WeatherDetail = ({detail, value}) => {
+  return (
+    <p className='flex justify-between border-b-1 pb-2 border-dashed border-gray-400'>
+        <span>{detail}</span>
+        <span>{value}</span>
+    </p>
+  )
+}
+
+export default WeatherDetail
